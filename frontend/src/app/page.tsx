d6fea1e49912b656fdd7e49d3bf59d739cbd32c6
paul-blackwell <paul@paulblackwell.design>
2024-12-18 08:26:15 +0000

    Part 2: Building Out The Home Page
    Actions:
    
    + Added HeroSection component
    + Added data for HeroSection component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from '@/components/ui/button';
+import qs from 'qs';
+import { HeroSection } from '@/components/custom/hero-section';
 
-async function getStrapiData(url: string) {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        'layout.hero-section': {
+          populate: {
+            image: {
+              fields: ['url', 'alternativeText'],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
   const baseUrl = 'http://localhost:1337';
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + url);
+    const response = await fetch(url.href);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -14,12 +38,11 @@ async function getStrapiData(url: string) {
 export default async function Home() {
   const strapiData = await getStrapiData('/api/home-page');
 
-  const { title, description } = strapiData.data;
+  const { title, description, blocks } = strapiData.data;
 
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
