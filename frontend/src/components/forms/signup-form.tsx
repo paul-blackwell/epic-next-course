b234b8ad4c76339e8cfc6b87b68e7de9d82509c3
paul-blackwell <paul@paulblackwell.design>
2024-12-22 17:11:49 +0000

    Next JS 15 Tutorial Part 4: How To Handle Login And Authentication in Next.js
    Actions:
    
    + How To Get Form State With useActionState Hook
    + Form Validation In Next.js with Zod

--- frontend/src/components/forms/signup-form.tsx
+++ frontend/src/components/forms/signup-form.tsx
@@ -15,9 +15,12 @@ import {
 
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { ZodErrors } from '@/components/custom/zod-errors';
 
 const INITIAL_STATE = {
-  data: 'test',
+  data: null,
+  zodErrors: null,
+  message: null,
 };
 
 export function SignupForm() {
@@ -25,6 +28,8 @@ export function SignupForm() {
     registerUserAction,
     INITIAL_STATE
   );
+
+  console.log(formState);
 
   return (
     <div className="w-full max-w-md">
@@ -45,6 +50,7 @@ export function SignupForm() {
                 type="text"
                 placeholder="username"
               />
+              <ZodErrors error={formState?.zodErrors?.username} />
             </div>
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
@@ -54,6 +60,7 @@ export function SignupForm() {
                 type="email"
                 placeholder="name@example.com"
               />
+              <ZodErrors error={formState?.zodErrors?.email} />
             </div>
 
             <div className="space-y-2">
@@ -64,6 +71,7 @@ export function SignupForm() {
                 type="password"
                 placeholder="password"
               />
+              <ZodErrors error={formState?.zodErrors?.password} />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
