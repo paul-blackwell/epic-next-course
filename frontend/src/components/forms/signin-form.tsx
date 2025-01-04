9a99f969e0fbac1e4de4ee10152d5234950326bd
paul-blackwell <paul@paulblackwell.design>
2025-01-04 18:25:14 +0000

    Next JS 15 Tutorial Part 4: How To Handle Login And Authentication in Next.js
    Actions:
    
    + How To Protect Your Routes in Next.js via Middleware

--- frontend/src/components/forms/signin-form.tsx
+++ frontend/src/components/forms/signin-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Link from 'next/link';
+import { useActionState } from 'react';
+import { loginUserAction } from '@/data/actions/auth-actions';
 
 import {
   CardTitle,
@@ -13,11 +15,25 @@ import {
 
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { ZodErrors } from '@/components/custom/zod-errors';
+import { StrapiErrors } from '@/components/custom/strapi-errors';
+import { SubmitButton } from '@/components/custom/submit-button';
+
+const INITIAL_STATE = {
+  zodErrors: null,
+  strapiErrors: null,
+  data: null,
+  message: null,
+};
 
 export function SigninForm() {
+  const [formState, formAction] = useActionState(
+    loginUserAction,
+    INITIAL_STATE
+  );
   return (
     <div className="w-full max-w-md">
-      <form>
+      <form action={formAction}>
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-3xl font-bold">Sign In</CardTitle>
@@ -34,6 +50,7 @@ export function SigninForm() {
                 type="text"
                 placeholder="username or email"
               />
+              <ZodErrors error={formState?.zodErrors?.identifier} />
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
@@ -43,10 +60,16 @@ export function SigninForm() {
                 type="password"
                 placeholder="password"
               />
+              <ZodErrors error={formState?.zodErrors?.password} />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
-            <button className="w-full">Sign In</button>
+            <SubmitButton
+              className="w-full"
+              text="Sign In"
+              loadingText="Loading"
+            />
+            <StrapiErrors error={formState?.strapiErrors} />
           </CardFooter>
         </Card>
         <div className="mt-4 text-center text-sm">
--- frontend/src/data/actions/auth-actions.ts
+++ frontend/src/data/actions/auth-actions.ts
@@ -2,7 +2,11 @@
 import { z } from 'zod';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import { registerUserService } from '@/data/services/auth-service';
+
+import {
+  registerUserService,
+  loginUserService,
+} from '@/data/services/auth-service';
 
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
@@ -35,7 +39,8 @@ export async function registerUserAction(prevState: any, formData: FormData) {
     return {
       ...prevState,
       zodErrors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing required fields, failed to register',
+      strapiErrors: null,
+      message: 'Missing Fields. Failed to Register.',
     };
   }
 
@@ -46,7 +51,7 @@ export async function registerUserAction(prevState: any, formData: FormData) {
       ...prevState,
       strapiErrors: null,
       zodErrors: null,
-      message: 'Oops, something went wrong. Please try again.',
+      message: 'Ops! Something went wrong. Please try again.',
     };
   }
 
@@ -55,11 +60,79 @@ export async function registerUserAction(prevState: any, formData: FormData) {
       ...prevState,
       strapiErrors: responseData.error,
       zodErrors: null,
-      message: 'Failed to register.',
+      message: 'Failed to Register.',
     };
   }
 
   const cookieStore = await cookies();
   cookieStore.set('jwt', responseData.jwt, config);
+
   redirect('/dashboard');
+}
+
+const schemaLogin = z.object({
+  identifier: z
+    .string()
+    .min(3, {
+      message: 'Identifier must have at least 3 or more characters',
+    })
+    .max(20, {
+      message: 'Please enter a valid username or email address',
+    }),
+  password: z
+    .string()
+    .min(6, {
+      message: 'Password must have at least 6 or more characters',
+    })
+    .max(100, {
+      message: 'Password must be between 6 and 100 characters',
+    }),
+});
+
+export async function loginUserAction(prevState: any, formData: FormData) {
+  const validatedFields = schemaLogin.safeParse({
+    identifier: formData.get('identifier'),
+    password: formData.get('password'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Login.',
+    };
+  }
+
+  const responseData = await loginUserService(validatedFields.data);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: 'Ops! Something went wrong. Please try again.',
+    };
+  }
+
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: 'Failed to Login.',
+    };
+  }
+
+  console.log(responseData, 'responseData');
+
+  const cookieStore = await cookies();
+  cookieStore.set('jwt', responseData.jwt, config);
+
+  redirect('/dashboard');
+}
+
+export async function logoutAction() {
+  const cookieStore = await cookies();
+  cookieStore.set('jwt', '', { ...config, maxAge: 0 });
+  redirect('/');
 }
